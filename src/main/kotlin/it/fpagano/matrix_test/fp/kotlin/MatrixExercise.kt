a23a646f04a6a01a1ec81592a929e634c8e7f426
Felice Pagano <felice.pagano@eidosmedia.com>
2019-11-02 02:15:40 +0100

    add kotlin implementation

---
+++ src/main/kotlin/it/fpagano/matrix_test/fp/kotlin/MatrixExercise.kt
@@ -0,0 +1,20 @@
+package it.fpagano.matrix_test.fp.kotlin
+
+import java.util.Optional
+
+class MatrixExercise<T>(private val unit: T, private val operation: (acc: T, T) -> T) {
+
+    fun find(matrix: List<List<T>>) = generateSequence(0) { it + 1 }.take(matrix.size).toList()
+            .flatMap { shiftRight(it, matrix[it].map { e -> Optional.of(e) }).withIndex() }
+            .filter { it.value.isPresent }
+            .groupBy({ it.index }, { it.value.get() })
+            .values.map { it.fold(unit, operation) }
+
+
+    private tailrec fun <T> shiftRight(n: Int, list: List<Optional<T>>): List<Optional<T>> = when (n) {
+        0 -> list
+        else -> shiftRight(n - 1, Optional.empty<T>() + list)
+    }
+
+    operator fun <T> Optional<T>.plus(xs: List<Optional<T>>): List<Optional<T>> = listOf(this) + xs
+}
---
+++ src/test/kotlin/it/fpagano/matrix_test/fp/kotlin/MatrixExerciseTest.kt
@@ -0,0 +1,41 @@
+package it.fpagano.matrix_test.fp.kotlin
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class MatrixExerciseTest {
+
+    @Test
+    fun findStrings() {
+        val matrix = listOf(
+                listOf("1", "A", "5", "7", "B", "N"),
+                listOf("2", "B", "6", "8", "K", "R"),
+                listOf("5", "Q", "N", "3", "B", "1")
+        )
+
+        val expectedResult = listOf("1", "A2", "5B5", "76Q", "B8N", "NK3", "RB", "1")
+
+        val matrixStringResolver = MatrixExercise("", String::plus)
+        val result = matrixStringResolver.find(matrix)
+
+        Assertions.assertEquals(expectedResult, result)
+
+    }
+
+    @Test
+    fun findIntsSum() {
+        val matrix = listOf(
+                listOf(1, 2),
+                listOf(3, 4),
+                listOf(5, 6)
+        )
+
+        val expectedResult = listOf(1, 5, 9, 6)
+
+        val matrixIntSumResolver = MatrixExercise(0, Int::plus)
+        val result = matrixIntSumResolver.find(matrix)
+
+        Assertions.assertEquals(expectedResult, result)
+
+    }
+}
